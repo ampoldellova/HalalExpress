ab3e8ae61370772807e837ceca5f935af38c4ebb
John Paul Dellova <dellova36@gmail.com>
2025-03-15 14:17:45 +0800

    Payment Method

--- frontend/screens/CheckoutPage.jsx
+++ frontend/screens/CheckoutPage.jsx
@@ -39,6 +39,7 @@ const CheckoutPage = () => {
     const [selectedAddressLat, setSelectedAddressLat] = useState(null);
     const [selectedAddressLng, setSelectedAddressLng] = useState(null);
     const [selectedDeliveryOption, setSelectedDeliveryOption] = useState(null);
+    const [selectedPaymentMethod, setSelectedPaymentMethod] = useState(null);
     const [distanceTime, setDistanceTime] = useState({});
     const [deliveryFee, setDeliveryFee] = useState(selectedDeliveryOption === 'standard' ? distanceTime.finalPrice : 0);
     const [editProfile, setEditProfile] = useState(false);
@@ -407,6 +408,66 @@ const CheckoutPage = () => {
                             </View>
                         )}
 
+                    </View>
+
+                    <View style={{ borderColor: COLORS.gray2, height: 'auto', borderWidth: 1, borderRadius: 10, padding: 10, marginBottom: 20 }}>
+                        <View style={{ flexDirection: 'row', alignItems: 'center', marginTop: 10 }}>
+                            <Image source={require('../assets/images/payment.png')} style={{ width: 25, height: 25 }} />
+                            <Text style={{ fontFamily: 'bold', fontSize: 18, marginLeft: 5 }}>Payment Method</Text>
+                        </View>
+
+                        <TouchableOpacity
+                            onPress={() => { selectedPaymentMethod === 'cod' ? setSelectedPaymentMethod(null) : setSelectedPaymentMethod('cod') }}
+                            style={{
+                                marginTop: 10,
+                                borderWidth: 1,
+                                padding: 10,
+                                borderRadius: 10,
+                                borderColor: selectedPaymentMethod === 'cod' ? COLORS.primary : COLORS.gray2,
+                                opacity: restaurant?.delivery ? 1 : 0.5
+                            }}>
+                            <View style={{ flexDirection: 'row', alignItems: 'center', justifyContent: 'space-between', width: '100%' }}>
+                                <View style={{ flexDirection: 'row', alignItems: 'center', height: 30 }}>
+                                    <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+                                        <Image source={require('../assets/images/COD.png')} style={{ width: 20, height: 20, marginLeft: 2.5 }} />
+                                        <Text style={{ fontFamily: 'medium', fontSize: 16, marginLeft: 10, color: COLORS.black }}>Cash On Delivery</Text>
+                                    </View>
+                                </View>
+                            </View>
+
+                            {selectedPaymentMethod === 'cod' && (
+                                <Text style={{ fontFamily: 'regular', fontSize: 12, color: COLORS.gray }}>
+                                    Consider payment upon ordering for contactless delivery. You cant pay by a card to the rider upon delivery.
+                                </Text>
+                            )}
+
+                        </TouchableOpacity>
+
+                        <TouchableOpacity
+                            onPress={() => { selectedPaymentMethod === 'gcash' ? setSelectedPaymentMethod(null) : setSelectedPaymentMethod('gcash') }}
+                            style={{
+                                marginTop: 10,
+                                borderWidth: 1,
+                                padding: 10,
+                                borderRadius: 10,
+                                borderColor: selectedPaymentMethod === 'gcash' ? COLORS.primary : COLORS.gray2,
+                                opacity: restaurant?.delivery ? 1 : 0.5
+                            }}>
+                            <View style={{ flexDirection: 'row', alignItems: 'center', justifyContent: 'space-between', width: '100%' }}>
+                                <View style={{ flexDirection: 'row', alignItems: 'center', height: 30 }}>
+                                    <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+                                        <Image source={require('../assets/images/gcash.png')} style={{ width: 27, height: 22, marginLeft: 2.5 }} />
+                                        <Text style={{ fontFamily: 'medium', fontSize: 16, marginLeft: 10, color: COLORS.black }}>GCash</Text>
+                                    </View>
+                                </View>
+                            </View>
+
+                            {selectedPaymentMethod === 'gcash' && (
+                                <Text style={{ fontFamily: 'regular', fontSize: 12, color: COLORS.gray }}>
+                                    You will be redirected to GCash after checkout. After you've performed the payment, you will be redirected back to HalalExpress.
+                                </Text>
+                            )}
+                        </TouchableOpacity>
                     </View>
 
                     <View style={{ borderColor: COLORS.gray2, height: 'auto', borderWidth: 1, borderRadius: 10, padding: 10 }}>
