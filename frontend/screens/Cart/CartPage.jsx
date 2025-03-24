4ee852faef7ab140b8dd760446a91ea58d690685
John Paul Dellova <dellova36@gmail.com>
2025-03-24 22:22:50 +0800

    Refactored cart page

--- frontend/screens/Cart/CartPage.jsx
+++ frontend/screens/Cart/CartPage.jsx
@@ -18,6 +18,7 @@ import BackBtn from "../../components/BackBtn";
 import CartProducts from "../../components/Cart/CartProducts";
 import Button from "../../components/Button";
 import { getProfile } from "../../hook/helpers";
+import Loader from "../../components/Loader";
 
 const CartPage = () => {
   const navigation = useNavigation();
@@ -26,6 +27,7 @@ const CartPage = () => {
   const [vendorCart, setVendorCart] = useState({});
   const [cartItems, setCartItems] = useState([]);
   const [vendorCartItems, setVendorCartItems] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   const fetchProfile = async () => {
     const profile = await getProfile();
@@ -41,26 +43,17 @@ const CartPage = () => {
         },
       };
 
-      const response = await axios.get(`${baseUrl}/api/cart/`, config);
-      setCartItems(response.data.cartItems);
-      setCart(response.data.cart);
-    } catch (error) {
-      console.log(error.message);
-    }
-  };
+      const endpoint =
+        user?.userType === "Vendor"
+          ? `${baseUrl}/api/cart/vendor/`
+          : `${baseUrl}/api/cart/`;
 
-  const getVendorCartItems = async () => {
-    try {
-      const token = await AsyncStorage.getItem("token");
-      const config = {
-        headers: {
-          Authorization: `Bearer ${JSON.parse(token)}`,
-        },
-      };
-
-      const response = await axios.get(`${baseUrl}/api/cart/vendor/`, config);
+      const response = await axios.get(endpoint, config);
       setVendorCartItems(response.data.cartItems);
+      setCartItems(response.data.cartItems);
       setVendorCart(response.data.vendorCart);
+      setCart(response.data.cart);
+      setLoading(false);
     } catch (error) {
       console.log(error.message);
     }
@@ -70,92 +63,96 @@ const CartPage = () => {
     React.useCallback(() => {
       fetchProfile();
       {
-        user && user.userType === "Vendor"
-          ? getVendorCartItems()
-          : getCartItems();
+        getCartItems();
       }
     }, [cartItems, vendorCartItems])
   );
 
   return (
     <SafeAreaView>
-      <View style={pages.viewOne}>
-        <View style={pages.viewTwo}>
-          {cartItems.length > 0 ? (
-            <View style={{ marginHorizontal: 20, marginTop: 15 }}>
-              <BackBtn onPress={() => navigation.goBack()} />
-              <Text style={styles.heading}>Your Cart</Text>
+      {loading ? (
+        <Loader />
+      ) : (
+        <View style={pages.viewOne}>
+          <View style={pages.viewTwo}>
+            {cartItems.length > 0 || vendorCartItems.length > 0 ? (
+              <View style={{ marginHorizontal: 20, marginTop: 15 }}>
+                <BackBtn onPress={() => navigation.goBack()} />
+                <Text style={styles.heading}>Your Cart</Text>
 
-              <FlatList
-                data={cartItems}
-                keyExtractor={(item) => item._id}
-                style={{ height: SIZES.height / 1.5 }}
-                renderItem={({ item }) => (
-                  <CartProducts item={item} getCartItems={getCartItems} />
-                )}
-              />
+                <FlatList
+                  data={
+                    user?.userType === "Vendor" ? vendorCartItems : cartItems
+                  }
+                  keyExtractor={(item) => item._id}
+                  style={{ height: SIZES.height / 1.5 }}
+                  renderItem={({ item }) => (
+                    <CartProducts item={item} getCartItems={getCartItems} />
+                  )}
+                />
 
-              <View
-                style={{
-                  flexDirection: "row",
-                  justifyContent: "space-between",
-                }}
-              >
-                <Text
+                <View
                   style={{
-                    fontFamily: "bold",
-                    fontSize: 18,
-                    textAlign: "center",
-                    marginTop: 10,
+                    flexDirection: "row",
+                    justifyContent: "space-between",
                   }}
                 >
-                  Total:
-                </Text>
-                <Text
-                  style={{
-                    fontFamily: "bold",
-                    fontSize: 18,
-                    textAlign: "center",
-                    marginTop: 10,
+                  <Text
+                    style={{
+                      fontFamily: "bold",
+                      fontSize: 18,
+                      textAlign: "center",
+                      marginTop: 10,
+                    }}
+                  >
+                    Total:
+                  </Text>
+                  <Text
+                    style={{
+                      fontFamily: "bold",
+                      fontSize: 18,
+                      textAlign: "center",
+                      marginTop: 10,
+                    }}
+                  >
+                    ₱ {cart.totalAmount.toFixed(2)}
+                  </Text>
+                </View>
+
+                <Button
+                  title="C H E C K O U T"
+                  isValid={true}
+                  onPress={() => {
+                    navigation.navigate("checkout-page", {
+                      cart,
+                      vendorCart,
+                      user,
+                    });
                   }}
-                >
-                  ₱ {cart.totalAmount.toFixed(2)}
-                </Text>
+                />
               </View>
-
-              <Button
-                title="C H E C K O U T"
-                isValid={true}
-                onPress={() => {
-                  navigation.navigate("checkout-page", {
-                    cart,
-                    vendorCart,
-                    user,
-                  });
-                }}
-              />
-            </View>
-          ) : (
-            <View style={styles.container}>
-              <Image
-                style={styles.image}
-                source={require("../../assets/images/cart.png")}
-              />
-              <Text style={styles.text}>
-                You havent added anything to your cart yet.
-              </Text>
-              <TouchableOpacity
-                style={styles.button}
-                onPress={() => {
-                  navigation.navigate("HomePage");
-                }}
-              >
-                <Text style={styles.buttonText}>Browse Items</Text>
-              </TouchableOpacity>
-            </View>
-          )}
+            ) : (
+              <View style={styles.container}>
+                <Image
+                  style={styles.image}
+                  source={require("../../assets/images/cart.png")}
+                />
+                <Text style={styles.text}>
+                  You havent added anything to your cart yet.
+                </Text>
+                <TouchableOpacity
+                  style={styles.button}
+                  onPress={() => {
+                    navigation.navigate("HomePage");
+                  }}
+                >
+                  <Text style={styles.buttonText}>Browse Items</Text>
+                </TouchableOpacity>
+              </View>
+            )}
+          </View>
         </View>
-      </View>
+      )}
     </SafeAreaView>
   );
 };
