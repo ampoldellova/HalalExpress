bf79e5049ca8759d35c9d03421d907270617cf6a
John Paul Dellova <dellova36@gmail.com>
2025-03-30 18:29:37 +0800

    Infinite Scroll on foods

--- frontend/components/Foods/FoodComponent.jsx
+++ frontend/components/Foods/FoodComponent.jsx
@@ -1,47 +1,47 @@
-import { Image, StyleSheet, Text, TouchableOpacity, View } from 'react-native'
-import React from 'react'
-import { COLORS, SIZES } from '../../styles/theme'
+import { Image, StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import React from "react";
+import { COLORS, SIZES } from "../../styles/theme";
 
 const FoodComponent = ({ item, onPress }) => {
-    return (
-        <TouchableOpacity style={styles.wrapper} onPress={onPress}>
-            <Image
-                source={{ uri: item.imageUrl.url }}
-                style={{
-                    width: SIZES.width - 230,
-                    height: SIZES.height / 5.8,
-                    borderRadius: 15,
-                    borderWidth: 1,
-                    borderColor: COLORS.gray2
-                }}
-            />
+  return (
+    <TouchableOpacity style={styles.wrapper} onPress={onPress}>
+      <Image
+        source={{ uri: item.imageUrl.url }}
+        style={{
+          width: SIZES.width - 230,
+          height: SIZES.height / 5.8,
+          borderRadius: 15,
+          borderWidth: 1,
+          borderColor: COLORS.gray2,
+        }}
+      />
 
-            <Text style={styles.small}>{item.restaurant.title}</Text>
-            <Text style={styles.heading}>{item.title}</Text>
-            <Text style={styles.small}>₱ {item.price}</Text>
-        </TouchableOpacity>
-    )
-}
+      <Text style={styles.small}>{item.restaurant.title}</Text>
+      <Text style={styles.heading}>{item.title}</Text>
+      <Text style={styles.small}>₱ {item.price}</Text>
+    </TouchableOpacity>
+  );
+};
 
-export default FoodComponent
+export default FoodComponent;
 
 const styles = StyleSheet.create({
-    wrapper: {
-        marginRight: 15,
-        borderColor: COLORS.lightWhite,
-        padding: 8,
-        borderRadius: 16
-    },
-    heading: {
-        fontSize: 14,
-        fontFamily: "regular",
-        color: COLORS.black,
-        width: 120
-    },
-    small: {
-        fontSize: 12,
-        fontFamily: "regular",
-        color: COLORS.gray,
-        marginTop: 5
-    }
-})
+  wrapper: {
+    marginRight: 5,
+    borderColor: COLORS.lightWhite,
+    padding: 8,
+    borderRadius: 16,
+  },
+  heading: {
+    fontSize: 14,
+    fontFamily: "regular",
+    color: COLORS.black,
+    width: 120,
+  },
+  small: {
+    fontSize: 12,
+    fontFamily: "regular",
+    color: COLORS.gray,
+    marginTop: 5,
+  },
+});
--- frontend/components/Foods/Foods.jsx
+++ frontend/components/Foods/Foods.jsx
@@ -1,26 +1,44 @@
-import { View, FlatList } from 'react-native'
-import React from 'react'
-import FoodComponent from './FoodComponent'
-import { useNavigation } from '@react-navigation/native'
+import { View, FlatList } from "react-native";
+import React, { useState } from "react";
+import FoodComponent from "./FoodComponent";
+import { useNavigation } from "@react-navigation/native";
 
 const Foods = ({ foods }) => {
-    const navigation = useNavigation();
+  const navigation = useNavigation();
+  const [visibleFoods, setVisibleFoods] = useState(foods.slice(0, 3));
+  const [currentIndex, setCurrentIndex] = useState(3);
 
-    return (
-        <View style={{ marginLeft: 12, marginBottom: 10 }}>
-            <FlatList
-                data={foods}
-                horizontal
-                showsHorizontalScrollIndicator={false}
-                style={{ marginTop: 5, rowGap: 10 }}
-                scrollEnabled
-                keyExtractor={(item) => item._id.toString()}
-                renderItem={({ item }) => (
-                    <FoodComponent item={item} onPress={() => navigation.navigate('food-navigator', item)} />
-                )}
-            />
-        </View>
-    )
-}
+  const loadMoreFoods = () => {
+    if (currentIndex < foods.length) {
+      const nextIndex = Math.min(currentIndex + 3, foods.length);
+      setVisibleFoods((prevFoods) => [
+        ...prevFoods,
+        ...foods.slice(currentIndex, nextIndex),
+      ]);
+      setCurrentIndex(nextIndex);
+    }
+  };
 
-export default Foods
+  return (
+    <View style={{ marginLeft: 12, marginBottom: 10 }}>
+      <FlatList
+        data={visibleFoods}
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        style={{ marginTop: 5 }}
+        scrollEnabled
+        keyExtractor={(item) => item._id.toString()}
+        renderItem={({ item }) => (
+          <FoodComponent
+            item={item}
+            onPress={() => navigation.navigate("food-navigator", item)}
+          />
+        )}
+        onEndReached={loadMoreFoods}
+        onEndReachedThreshold={0.5}
+      />
+    </View>
+  );
+};
+
+export default Foods;
