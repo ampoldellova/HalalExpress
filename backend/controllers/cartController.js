4d7a2af6af3dd9718f298824aa560ae13ff62b15
John Paul Dellova <dellova36@gmail.com>
2025-04-20 23:32:12 +0800

    Refactored increment items in cart

--- backend/controllers/cartController.js
+++ backend/controllers/cartController.js
@@ -198,80 +198,138 @@ module.exports = {
   },
 
   incrementCartItemQuantity: async (req, res) => {
-    const userId = req.user.id;
-    const foodId = req.params.id;
-    try {
-      let cart = await Cart.findOne({ userId });
-
-      if (!cart) {
-        return res
-          .status(404)
-          .json({ status: false, message: "Cart not found" });
-      }
-
-      const existingItemIndex = cart.cartItems.findIndex(
-        (item) => item.foodId._id.toString() === foodId
-      );
-
-      if (existingItemIndex > -1) {
-        const item = cart.cartItems[existingItemIndex];
-        item.quantity += 1;
-        item.totalPrice += item.foodId.price;
-        cart.totalAmount += item.foodId.price;
-
-        await cart.save();
-        return res
-          .status(200)
-          .json({ status: true, message: "Quantity incremented successfully" });
-      } else {
-        return res
-          .status(404)
-          .json({ status: false, message: "Item not found in cart" });
-      }
-    } catch (error) {
-      res.status(500).json({ status: false, message: error.message });
-    }
-  },
-
-  decrementCartItemQuantity: async (req, res) => {
-    const userId = req.user.id;
-    const foodId = req.params.id;
+    const user = req.user;
+    const userId = user.id;
+    const itemId = req.params.id;
 
     try {
       let cart = await Cart.findOne({ userId });
 
-      if (cart) {
+      if (user.userType === "Vendor") {
         const existingItemIndex = cart.cartItems.findIndex(
-          (item) => item.foodId._id.toString() === foodId
+          (item) => item.productId._id.toString() === itemId
         );
 
         if (existingItemIndex > -1) {
           const item = cart.cartItems[existingItemIndex];
-          if (item.quantity > 1) {
-            item.quantity -= 1;
-            item.totalPrice -= item.foodId.price;
-            cart.totalAmount -= item.foodId.price;
+          item.quantity += 1;
+          item.totalPrice += item.productId.price;
+          cart.totalAmount += item.productId.price;
 
-            await cart.save();
-            return res.status(200).json({
-              status: true,
-              message: "Quantity decremented successfully",
-            });
-          } else {
-            return res.status(400).json({
-              status: false,
-              message: "Quantity cannot be less than 1",
-            });
-          }
+          await cart.save();
+          return res.status(200).json({
+            status: true,
+            message: "Quantity incremented successfully",
+          });
         } else {
           return res
             .status(404)
             .json({ status: false, message: "Item not found in cart" });
         }
       } else {
-        return res
-          .status(404)
-          .json({ status: false, message: "Cart not found" });
+        const existingItemIndex = cart.cartItems.findIndex(
+          (item) => item.foodId._id.toString() === itemId
+        );
+
+        if (existingItemIndex > -1) {
+          const item = cart.cartItems[existingItemIndex];
+          item.quantity += 1;
+          item.totalPrice += item.foodId.price;
+          cart.totalAmount += item.foodId.price;
+
+          await cart.save();
+          return res.status(200).json({
+            status: true,
+            message: "Quantity incremented successfully",
+          });
+        } else {
+          return res
+            .status(404)
+            .json({ status: false, message: "Item not found in cart" });
+        }
+      }
+    } catch (error) {
+      console.log(error);
+      res.status(500).json({ status: false, message: error.message });
+    }
+  },
+
+  decrementCartItemQuantity: async (req, res) => {
+    const user = req.user;
+    const userId = req.user.id;
+    const itemId = req.params.id;
+
+    try {
+      let cart = await Cart.findOne({ userId });
+
+      if (user.userType === "Vendor") {
+        if (cart) {
+          const existingItemIndex = cart.cartItems.findIndex(
+            (item) => item.foodId._id.toString() === foodId
+          );
+
+          if (existingItemIndex > -1) {
+            const item = cart.cartItems[existingItemIndex];
+            if (item.quantity > 1) {
+              item.quantity -= 1;
+              item.totalPrice -= item.foodId.price;
+              cart.totalAmount -= item.foodId.price;
+
+              await cart.save();
+              return res.status(200).json({
+                status: true,
+                message: "Quantity decremented successfully",
+              });
+            } else {
+              return res.status(400).json({
+                status: false,
+                message: "Quantity cannot be less than 1",
+              });
+            }
+          } else {
+            return res
+              .status(404)
+              .json({ status: false, message: "Item not found in cart" });
+          }
+        } else {
+          return res
+            .status(404)
+            .json({ status: false, message: "Cart not found" });
+        }
+      } else {
+        if (cart) {
+          const existingItemIndex = cart.cartItems.findIndex(
+            (item) => item.foodId._id.toString() === foodId
+          );
+
+          if (existingItemIndex > -1) {
+            const item = cart.cartItems[existingItemIndex];
+            if (item.quantity > 1) {
+              item.quantity -= 1;
+              item.totalPrice -= item.foodId.price;
+              cart.totalAmount -= item.foodId.price;
+
+              await cart.save();
+              return res.status(200).json({
+                status: true,
+                message: "Quantity decremented successfully",
+              });
+            } else {
+              return res.status(400).json({
+                status: false,
+                message: "Quantity cannot be less than 1",
+              });
+            }
+          } else {
+            return res
+              .status(404)
+              .json({ status: false, message: "Item not found in cart" });
+          }
+        } else {
+          return res
+            .status(404)
+            .json({ status: false, message: "Cart not found" });
+        }
       }
     } catch (error) {
       res.status(500).json({ status: false, message: error.message });
