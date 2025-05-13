29eb25f60e0a1e3fff2f6365c68c2f2c758962ae
John Paul Dellova <dellova36@gmail.com>
2025-05-13 23:06:08 +0800

    Store rider location in firestore database

---
+++ deliveryRider/config/firebase.js
@@ -0,0 +1,17 @@
+import { initializeApp } from "@react-native-firebase/app";
+import { getAuth } from "@react-native-firebase/auth";
+import { getFirestore } from "@react-native-firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyChi4RK2bphEVRwJ_Ma_GNmr2sljXEXeJM",
+  authDomain: "halalexpress-86dbb.firebaseapp.com",
+  projectId: "halalexpress-86dbb",
+  storageBucket: "halalexpress-86dbb.firebasestorage.app",
+  messagingSenderId: "239431572065",
+  appId: "1:239431572065:web:d9ec84ad5922158044a820",
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
+export const database = getFirestore();
