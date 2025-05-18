6ddef2c0aacd7c2e0c362d8547f5f2227027c6fb
John Paul Dellova <dellova36@gmail.com>
2025-05-18 11:33:41 +0800

    Deployed server

--- frontend/assets/common/baseUrl.js
+++ frontend/assets/common/baseUrl.js
@@ -7,10 +7,16 @@ let baseUrl = "";
 //     : (baseUrl = "http://192.168.254.114:6002");
 // }
 
+// {
+//   Platform.OS == "android"
+//     ? (baseUrl = "http://192.168.254.114:6002")
+//     : (baseUrl = "http://192.168.254.114:6002");
+// }
+
 {
   Platform.OS == "android"
-    ? (baseUrl = "http://192.168.254.114:6002")
-    : (baseUrl = "http://192.168.254.114:6002");
+    ? (baseUrl = "https://halalexpress.onrender.com")
+    : (baseUrl = "https://halalexpress.onrender.com");
 }
 
 export default baseUrl;
