368b91fa1683fb581f067a980d8ebdff3ba69013
John Paul DELLOVA <John.Dellova@plus.local>
2025-05-27 17:29:19 +0800

    Verification Page

--- website/src/screens/User/VerificationPage.jsx
+++ website/src/screens/User/VerificationPage.jsx
@@ -1,15 +1,56 @@
-import { Box, Container, Typography } from "@mui/material";
-import React from "react";
-import { useLocation } from "react-router-dom";
+import {
+  Box,
+  Button,
+  CircularProgress,
+  Container,
+  Typography,
+} from "@mui/material";
+import React, { useState } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
 import { COLORS } from "../../styles/theme";
 import Lottie from "lottie-react";
 import mail from "../../assets/anime/mail.json";
+import OtpInput from "react-otp-input";
+import axios from "axios";
+import { toast } from "react-toastify";
 
 const VerificationPage = () => {
+  const navigation = useNavigate();
+  const [loading, setLoading] = useState(false);
   const location = useLocation();
   const { data } = location.state || {};
+  const [otp, setOtp] = useState("");
 
-  console.log("VerificationPage data:", data);
+  const verifyUser = async () => {
+    setLoading(true);
+
+    try {
+      const endpoint = `http://localhost:6002/verify-otp`;
+      const verification = {
+        email: data.email,
+        otp: otp,
+      };
+
+      const response = await axios.post(endpoint, verification);
+
+      if (response.status === 201) {
+        setLoading(false);
+        toast.success(
+          "Verification Successful ✅ Your account has been verified successfully."
+        );
+        navigation("/");
+      } else {
+        setLoading(false);
+        toast.error(
+          "Verification Failed",
+          response.data.error || "Please try again."
+        );
+      }
+    } catch (error) {
+      setLoading(false);
+      toast.error("Error ❌", error.message);
+    }
+  };
 
   return (
     <Box sx={{ backgroundColor: COLORS.offwhite, height: "100vh" }}>
@@ -20,13 +61,12 @@ const VerificationPage = () => {
           flexDirection: "column",
           alignItems: "center",
           justifyContent: "center",
-          height: "100%",
         }}
       >
         <Lottie
           animationData={mail}
           loop={true}
-          style={{ width: 300, height: 300 }}
+          style={{ width: 300, height: 300, marginTop: 200 }}
         />
 
         <Typography
@@ -46,12 +86,52 @@ const VerificationPage = () => {
             textAlign: "center",
             color: COLORS.gray,
             mt: 2,
+            mb: 4,
             width: 800,
           }}
         >
           We have sent a verification code to your email address. Please enter
           the code to verify your account.
         </Typography>
+
+        <OtpInput
+          value={otp}
+          onChange={setOtp}
+          numInputs={6}
+          renderSeparator={<span style={{ width: "20px" }} />}
+          renderInput={(props) => <input {...props} />}
+          inputStyle={{
+            width: "80px",
+            height: "100px",
+            fontSize: "44px",
+            borderRadius: "18px",
+            border: `2px solid ${COLORS.primary}`,
+            backgroundColor: "transparent",
+            color: COLORS.black,
+            textAlign: "center",
+          }}
+        />
+
+        <Button
+          variant="contained"
+          sx={{
+            mt: 8,
+            px: 4,
+            py: 2,
+            fontSize: 20,
+            fontFamily: "bold",
+            borderRadius: "18px",
+            backgroundColor: COLORS.primary,
+            width: "200px",
+          }}
+          onClick={() => verifyUser()}
+        >
+          {loading ? (
+            <CircularProgress size={34} color="white" />
+          ) : (
+            "V E R I F Y"
+          )}
+        </Button>
       </Container>
     </Box>
   );
