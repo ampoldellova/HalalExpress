bda800a1e03d27ecdc6ded7eb048fbbbb7059361
John Paul Dellova <dellova36@gmail.com>
2025-05-31 11:09:31 +0800

    Updated configuration

--- mobile/config/firebase.js
+++ mobile/config/firebase.js
@@ -7,12 +7,12 @@ import Constants from "expo-constants";
 const { firebase } = Constants.expoConfig.extra;
 
 const firebaseConfig = {
-    apiKey: "AIzaSyChi4RK2bphEVRwJ_Ma_GNmr2sljXEXeJM",
-    authDomain: "halalexpress-86dbb.firebaseapp.com",
-    projectId: "halalexpress-86dbb",
-    storageBucket: "halalexpress-86dbb.firebasestorage.app",
-    messagingSenderId: "239431572065",
-    appId: "1:239431572065:web:d9ec84ad5922158044a820",
+  apiKey: "AIzaSyChi4RK2bphEVRwJ_Ma_GNmr2sljXEXeJM",
+  authDomain: "halalexpress-86dbb.firebaseapp.com",
+  projectId: "halalexpress-86dbb",
+  storageBucket: "halalexpress-86dbb.firebasestorage.app",
+  messagingSenderId: "239431572065",
+  appId: "1:239431572065:web:d9ec84ad5922158044a820",
 };
 
 const app = initializeApp(firebaseConfig);
