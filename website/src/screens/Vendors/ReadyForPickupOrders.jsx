b063534085a3bea30588783ecfa8b524f9e075b5
John Paul DELLOVA <John.Dellova@plus.local>
2025-06-05 13:19:01 +0800

    Deployment

--- website/src/screens/Vendors/ReadyForPickupOrders.jsx
+++ website/src/screens/Vendors/ReadyForPickupOrders.jsx
@@ -324,7 +324,7 @@ const ReadyForPickupOrders = ({ readyForPickupOrders }) => {
             {selectedOrder && (
               <QRCodeSVG
                 size={228}
-                value={`https://halal-express.vercel.app/accept-order/${selectedOrder?._id}`}
+                value={`https://halal-express-rd5v.vercel.app/accept-order/${selectedOrder?._id}`}
                 imageSettings={{
                   src: icon,
                   x: undefined,
@@ -345,7 +345,7 @@ const ReadyForPickupOrders = ({ readyForPickupOrders }) => {
                 my: 2,
               }}
             >
-              {`https://halal-express.vercel.app/accept-order/${selectedOrder?._id}`}
+              {`https://halal-express-rd5v.vercel.app/accept-order/${selectedOrder?._id}`}
             </Typography>
           </Box>
         </Box>
